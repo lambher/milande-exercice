c4b123111c540318073fa7cb837ccf73544918e8
Lambert Teisserenc <lambert.teisserenc@braineet.com>
2020-11-15 15:52:50 +0100

    add main

---
+++ main.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+        "firstName": "Milande",
+        "lastName": "Teisserenc-Bonesteve",
+        "age": 26,
+        "email": "milande.teisserenc@gmail.com",
+        "password": "xweoENFEF8"
+    },
+    {
+        "firstName": "Lambert",
+        "lastName": "Teisserenc-Bonesteve",
+        "age": 27,
+        "email": "lambert.teisserenc@gmail.com",
+        "password": "ef68e4f6EF8"
+    },
+    {
+        "firstName": "Remi",
+        "lastName": "Dasse",
+        "age": 31,
+        "email": "remi.dasse@gmail.com",
+        "password": "984fFexfG"
+    },
+    {
+        "firstName": "Berline",
+        "lastName": "Dasse",
+        "age": 31,
+        "email": "berline.dasse@gmail.com",
+        "password": "qwe6fwG5"
+    },
+];
+
+function orderByAge(users) {
+
+}
+
+function orderByFirstName(users) {
+
+}
+
+function orderByLastNameAndAge(users) {
+
+}
+
+function filterByLastName(users, lastName) {
+
+}
+
+function clearPassword(users) {
+
+}
